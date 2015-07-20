13eb99ce8e4a73fa575e88adc6a3193e490e01c4
Daniel <danielkou131@gmail.com>
2015-07-19 21:17:38 -0400

    initial commit

--- main.js
+++ main.js
@@ -1,92 +1,97 @@
 window.onload = function(){
-//    var snow = 0;
-//    var start;
-//  
-//    var checkPageButton = document.getElementById("button");
-//    checkPageButton.addEventListener('click', function() {
-//      snow++;
-//      snow = snow%2;
-//      
-//      if(snow == 1) start = setInterval(draw, 33);
-//      else clearInterval(start);
-//    });
+  var start = 0;
+  var animation;
   
+  //make canvas
+  var canvas = document.createElement("canvas");
+  canvas.id = "canvas"
+  canvas.style.position = "absolute";
+  canvas.style.left = "0px";
+  canvas.style.top = "0px";
+  canvas.style.zIndex = "10000";
+  canvas.style.pointerEvents = "none";
+
+
+  var body = document.body;
+  var html = document.documentElement;
+
+  var W = window.innerWidth;
+  var H = Math.max(body.scrollHeight, body.offsetHeight, 
+                   html.clientHeight, html.scrollHeight, html.offsetHeight);
+
+  canvas.width = W;
+  canvas.height = H;
+
   
-	var canvas = document.createElement("canvas");
-	canvas.id = "canvas"
-	canvas.style.position = "absolute";
-	canvas.style.left = "0px";
-	canvas.style.top = "0px";
-	canvas.style.zIndex = "1000000";
-	canvas.style.pointerEvents = "none";
+  body.appendChild(canvas);
 
+  var ctx = canvas.getContext("2d");
 
-	var body = document.body;
-    var html = document.documentElement;
+  var mp = 500; //max snowflakes
+  var particles = [];
 
-	var W = window.innerWidth;
-	var H = Math.max(body.scrollHeight, body.offsetHeight, 
-                       html.clientHeight, html.scrollHeight, html.offsetHeight);
+  for(var i=0; i<mp; i++){
+    particles.push({
+      x: Math.random()*W, //x-coord
+      y: Math.random()*H, //y-coord
+      r: Math.random()*4+1, //radius
+      d: Math.random()*mp //falling speed
+    })
+  }
+  
+  //listen for browser action click
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if( request.message == "clicked_browser_action" ) {
+      start++;
+      start = start%2;
+      
+      //toggle animation
+      if(start == 1){
+        animation = setInterval(draw, 33);
+      }
+      else{
+        clearInterval(animation);
+        ctx.clearRect(0,0,W,H);
+      }
+    }
+  });
 
-	canvas.width = W;
-	canvas.height = H;
+  //updating function to animate
+  function draw(){
+    ctx.clearRect(0,0,W,H);
+    ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
+    ctx.beginPath();
 
-	
-	body.appendChild(canvas);
+      for(var i=0; i<mp; i++){
+        var p = particles[i];
+        ctx.moveTo(p.x, p.y);
+        ctx.arc(p.x, p.y, p.r, 0, Math.PI*2, true);			
+      }
+      ctx.fill();
+      snow();
+    }
 
-	var ctx = canvas.getContext("2d");
+  var angle = 0;
+  //updates snow coordinates
+  function snow(){
+    angle += 0.01;
+    for (var i=0; i<mp; i++){
+      var p = particles[i];
+      p.x += Math.sin(angle)*2.5;
+      p.y += Math.cos(angle+p.d) + 1 + p.r/2;
 
-	var mp = 100; //max snowflakes
-	var particles = [];
-
-	for(var i=0; i<mp; i++){
-		particles.push({
-			x: Math.random()*W, //x-coord
-			y: Math.random()*H, //y-coord
-			r: Math.random()*4+1, //radius
-			d: Math.random()*mp //falling speed
-		})
-	}
-
-	function draw(){
-		ctx.clearRect(0,0,W,H);
-		ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
-		ctx.beginPath();
-
-		for(var i=0; i<mp; i++){
-			var p = particles[i];
-			ctx.moveTo(p.x, p.y);
-			ctx.arc(p.x, p.y, p.r, 0, Math.PI*2, true);			
-		}
-		ctx.fill();
-		snow();
-	}
-
-	var angle = 0;
-	function snow(){
-		angle += 0.01;
-
-		for (var i=0; i<mp; i++){
-			var p = particles[i];
-
-			p.x += Math.sin(angle)*2.5;
-			p.y += Math.cos(angle+p.d) + 1 + p.r/2;
-
-			if(p.x>W+5 || p.x<-5 || p.y>H){
-				if (i%3 > 0){
-					particles[i] = {x: Math.random()*W,	y: -10, r: p.r, d: p.d};
-				}
-				else{
-					if (Math.sin(angle) > 0){
-						particles[i] = {x: -5,	y: Math.random()*H, r: p.r, d: p.d};
-					}
-					else{
-						particles[i] = {x: W+5,	y: Math.random()*H, r: p.r, d: p.d};
-					}
-				}
-			}
-		}
-	}
-  setInterval(draw, 33);
-	
+      if(p.x>W+5 || p.x<-5 || p.y>H){
+        if (i%3 > 0){
+        particles[i] = {x: Math.random()*W,	y: -10, r: p.r, d: p.d};
+        }
+        else{
+          //exit from right
+          if (Math.sin(angle) > 0) particles[i] = {x: -5,	y: Math.random()*H, r: p.r, d: p.d};
+          //exit from left
+          else particles[i] = {x: W+5,	y: Math.random()*H, r: p.r, d: p.d};
+        }
+      }
+    }
+  }
 }
+//Icons made by Freepik from www.flaticon.com 
